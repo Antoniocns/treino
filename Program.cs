2aa072f4daba1e7d0d45aa9164ff6d12c8e84e7f
Antoniocns <acnspeixe@gmail.com>
2021-08-22 16:41:21 -0300

    finalizando

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estou treinando acesso github");
+        }
+    }
+}
